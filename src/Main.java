4bf04bbca2b9295c53bf20e652f31c5e12a402ee
fanqiangye <fanqiang_ye@hotmail.com>
2023-12-01 19:09:56 +0100

    Eerste progressie opslaan huiswerk

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
+        Scanner scanner = new Scanner(System.in);
+
+        Integer[]numeric = {1,2,3,4,5,6,7,8,9,0};
+        String[]alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
+
+        Translator translator = new Translator(numeric, alphabetic);
 
 
     }
---
+++ src/Translator.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Translator {
+
+    HashMap <Integer, String> numericAlpha = new HashMap<>();
+
+    public Translator (Integer[] numeric, String[] alphabetic){
+        for (int i = 0; i < numeric.length; i++) {
+            numericAlpha.put(numeric[0], alphabetic[0]);
+        }
+    }
+
+}
